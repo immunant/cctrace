039e8d0b4db2261d18fb64499b32dcecaf99fc9f
Per Larsen <perl@immunant.com>
2018-05-23 17:08:46 -0700

    Improve coloring and robustness.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # cctrace
-Trace invocations of compiler, linker, and other build tools
+Trace invocations of compiler, linker, and other build tools.
 
 Note: `cctrace` relies on `sysdig` which is only fully supported on Linux.
+
+## TODOs:
+
+- [ ] Handle multi-line commands 
+- [ ] Improve classification of tools
+    - [ ] detect non-system compilers
+    - [ ] detect selfrando
+- [ ] Print process tree    
--- cctrace.py
+++ cctrace.py
@@ -1,17 +1,68 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import shutil
 
-# Terminal escape codes
-OKBLUE = '\033[94m'
-OKGREEN = '\033[92m'
-WARNING = '\033[93m'
+# Terminal color codes
+NO_COLOR = '\033[0m'
+DGRAY = '\033[90m'
 FAIL = '\033[91m'
-NO_COLOUR = '\033[0m'
+LGREEN = '\033[92m'
+LYELLOW = '\033[93m'
+LBLUE = '\033[94m'
 
 # Terminal dimensions
 COLUMNS, LINES = shutil.get_terminal_size((80, 20))
+
+# Known binaries
+# TODO: use a tree-like structure here?
+COLOR_MAP = {
+    # utilities -> GRAY
+    '/bin/uname': DGRAY,
+    '/bin/sh': DGRAY,
+    '/bin/rm': DGRAY,
+    '/usr/bin/less': DGRAY,
+    '/bin/bash': DGRAY,
+    '/bin/tar': DGRAY,
+    '/bin/gzip': DGRAY,
+    '/bin/gzip': DGRAY,
+    '/bin/sed': DGRAY,
+    '/usr/bin/diff': DGRAY,
+    '/usr/bin/dpkg': DGRAY,
+    '/usr/bin/dpkg-query': DGRAY,
+    '/usr/bin/awk': DGRAY,
+    '/usr/bin/gawk': DGRAY,
+    '/usr/bin/env': DGRAY,
+    '/usr/bin/find': DGRAY,
+    '/usr/bin/ar': DGRAY,
+    '/usr/bin/nice': DGRAY,
+    '/usr/bin/sort': DGRAY,
+    '/usr/bin/print': DGRAY,
+    '/usr/bin/wget': DGRAY,
+    '/usr/bin/which': DGRAY,
+    # host translators -> YELLOW
+    '/usr/bin/yasm': LYELLOW,
+    '/usr/bin/as': LYELLOW,
+    '/usr/bin/ranlib': LYELLOW,
+    '/usr/bin/ld': LYELLOW,
+    '/usr/bin/gcc': LYELLOW,
+    '/usr/lib/gcc/x86_64-linux-gnu/7/cc1': LYELLOW,
+    '/usr/lib/gcc/x86_64-linux-gnu/7/collect2': LYELLOW,
+    '/usr/bin/g++': LYELLOW,
+    '/usr/bin/clang': LYELLOW,
+    '/usr/bin/clang++': LYELLOW,
+    # build tools
+    '/usr/bin/make': LBLUE,
+    '/usr/bin/cmake': LBLUE,
+    '/usr/bin/ninja': LBLUE,
+    '/usr/bin/pkg-config': LBLUE,
+    # scripting engines -> BLUE
+    '/usr/bin/python2.7': LBLUE,
+    '/usr/bin/python3': LBLUE,
+    '/usr/bin/perl': LBLUE,
+    '/usr/bin/tclsh8.6': LBLUE,
+}
 
 LASTLINES = 0  # how many lines did last call to update_display print?
 
@@ -24,10 +75,12 @@ def update_display(binaries):
 
     maxlen = max([len(b) for b in binaries])
 
-    def pad(instr):
+    def format(instr):
+        if instr in COLOR_MAP:
+            instr = COLOR_MAP[instr] + instr + NO_COLOR
         return instr.ljust(COLUMNS)
 
-    lines = [pad(b) for b in sorted(binaries)]
+    lines = [format(b) for b in sorted(binaries)]
     LASTLINES = len(lines)
     sys.stdout.write("\n".join(lines))
 
@@ -39,7 +92,12 @@ if __name__ == "__main__":
     try:
         for line in sys.stdin:
             atoms = line.split()
-            binary = atoms[0]
+            assert atoms[0].startswith("filename="), "unexpected: " + line
+            binary = atoms[0][9:]
+            # we don't monitor whether execve fails or not, so we
+            # skip calls that cannot succeed.
+            if not os.path.exists(binary):
+                continue
             BINARIES.add(binary)
             if len(BINARIES) > BIN_COUNT:
                 BIN_COUNT += 1
