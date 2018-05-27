bf84b84085bb15beecf4be031da44746964b4f7f
perl <perl@immunant.com>
2018-05-27 03:21:48 -0700

    Listen to both execve and clone exit events.

--- cctrace.py
+++ cctrace.py
@@ -12,6 +12,7 @@ from collections import defaultdict
 from anytree import Node, RenderTree
 from anytree.node.exceptions import TreeError
 from anytree.render import AsciiStyle, ContStyle
+from anytree.iterators.levelordergroupiter import LevelOrderGroupIter
 
 # Terminal color codes
 NO_COLOR = '\033[0m'
@@ -69,8 +70,10 @@ COLOR_MAP = {
     '/usr/bin/gcc': LYELLOW,
     # TODO: use regexes instead of per-version keys
     '/usr/lib/gcc/x86_64-linux-gnu/4.8/cc1': LYELLOW,
+    '/usr/lib/gcc/x86_64-linux-gnu/4.8/cc1plus': LYELLOW,
     '/usr/lib/gcc/x86_64-linux-gnu/4.8/collect2': LYELLOW,
     '/usr/lib/gcc/x86_64-linux-gnu/7/cc1': LYELLOW,
+    '/usr/lib/gcc/x86_64-linux-gnu/7/cc1plus': LYELLOW,
     '/usr/lib/gcc/x86_64-linux-gnu/7/collect2': LYELLOW,
     '/usr/lib/llvm-3.6/bin/clang': LYELLOW,
     '/usr/lib/llvm-3.6/bin/clang++': LYELLOW,
@@ -80,6 +83,11 @@ COLOR_MAP = {
     # build tools
     '/usr/bin/make': LBLUE,
     '/usr/bin/cmake': LBLUE,
+    '/usr/bin/ccmake': LBLUE,
+    '/usr/bin/ctest': LBLUE,
+    '/usr/bin/cpack': LBLUE,
+    '/usr/bin/qmake': LBLUE,
+    '/usr/bin/scons': LBLUE,
     '/usr/bin/ninja': LBLUE,
     '/usr/bin/pkg-config': LBLUE,
     '/usr/bin/bear': LBLUE,
@@ -94,10 +102,12 @@ PP = pprint.PrettyPrinter(indent=2)
 LANG_IS_UTF8 = os.environ.get('LANG', '').lower().endswith('utf-8')
 STY = ContStyle if LANG_IS_UTF8 else AsciiStyle
 
+UNKNOWN_PROC_LABEL = "[unknown executable]"
+UNKNOWN_PROC_COLOR = FAIL
 
 NODES = dict()
 
-def parse_exit_evt(e: dict) -> dict:
+def parse_evt_args(e: dict) -> dict:
     def split_kv(s: str) -> str:
         try:
             i = s.index('=')
@@ -105,21 +115,27 @@ def parse_exit_evt(e: dict) -> dict:
         except ValueError:
             print("no = in :" + s)
 
-    def parse_env(env: str) -> dict:
-        # base64 str -> bytes -> str
-        env = base64.b64decode(env).decode("utf-8")
-        env = [split_kv(a) for a in env.split('\x00') if a]
-        return {k: v for k, v in env}
+    # def parse_env(env: str) -> dict:
+    #     # base64 str -> bytes -> str
+    #     env = base64.b64decode(env).decode("utf-8")
+    #     env = [split_kv(a) for a in env.split('\x00') if a]
+    #     return {k: v for k, v in env}
 
-    args = e['evt.args'].rstrip().split(' ')
-    args = e.pop('evt.args').rstrip().split()
+    # TODO: handle something like:
+    # 'res=61211(code) exe=/usr/share/code/code --unity-launch args= '
+    #           'tid=11013(code) pid=11013(code) ptid=1751(init) cwd= '
+    #           'fdlimit=4096 pgft_maj=146 pgft_min=61427 vm_size=1421332 '
+    #           'vm_rss=106824 vm_swap=0 comm=code '
+
+    args = e.pop('evt.args').rstrip().split(' ')
     igno = ['cgroups', 'fdlimit', 'vm_size', 'vm_rss', 'vm_swap', 'pgft_maj', 'pgft_min']
     args = {k: v for k, v in [split_kv(a) for a in args] if k not in igno}
 
     # parse out env variables
     # args['env'] = parse_env(args['env'])
-    args.pop('env')
+    if 'env' in args: args.pop('env')
     e.update(args)
+
     return e
 
 
@@ -127,83 +143,96 @@ class CCNode(Node):
     separator = "|"
 
 
-def handle_events(enterevt: dict, exitevt: dict):
-    exitevt = parse_exit_evt(exitevt)
+def _parse_pid(s: str):
+    """
+    123(ab) -> 123
+    """
+    try:
+        return int(s[:s.index('(')]) 
+    except ValueError:  # '(' not found
+        return int(s)
 
-    # ignore non-successful events
-    if exitevt.pop('res') != '0':
-        return
+def handle_execve(exitevt: dict):
+    """
+    TODO: deal with PID wrap-around.
+    """
+    # exitevt = parse_evt_args(exitevt)
 
-    # PP.pprint(enterevt)
-    # print("-------------")
-    # PP.pprint(exitevt)
-    # quit(1)
+    parent_pid, child_pid = int(exitevt['proc.ppid']), int(exitevt['proc.pid']) 
+    child = exitevt['proc.exepath']
+    ccolor =  COLOR_MAP.get(child, NO_COLOR)
 
-    parent, parent_pid = enterevt['proc.exepath'], int(enterevt['proc.ppid'])
-    child, child_pid = exitevt['proc.exepath'], exitevt['pid']
-    child_pid = int(child_pid[:child_pid.index('(')])  # 123(ab) -> 123
-    assert parent_pid != child_pid
+    # sometimes 'proc.exepath' is blank. might be during high load.
+    if not child.strip():
+       child = UNKNOWN_PROC_LABEL
+       ccolor = UNKNOWN_PROC_COLOR
 
-    pcolor = COLOR_MAP.get(parent, NO_COLOR)
-    rnode = CCNode(parent, count=1, color=pcolor, pid=parent_pid)
+    # the lookup of the parent process can fail if the process was 
+    # started before we started running sysdig
+    rnode = CCNode(UNKNOWN_PROC_LABEL, color=UNKNOWN_PROC_COLOR, pid=parent_pid)
     pnode = NODES.setdefault(parent_pid, rnode)
 
-    # deduplicate child nodes
-    for cnode in pnode.children:
-        if cnode.name == child:
-            # found existing child!
-            cnode.count += 1
-            # associated with additional child pid
-            NODES[child_pid] = cnode
-            break
-    else:  # didn't find existing child!
-        ccolor = COLOR_MAP.get(child, NO_COLOR)
-        NODES[child_pid] = CCNode(child,
-                                  parent=pnode,
-                                  count=1,
-                                  color=ccolor,
-                                  pid=child_pid)
+    cnode = NODES.get(child_pid, None)
+    if cnode:
+        cnode.name = child
+        cnode.color = ccolor
+    else:
+        # happens if a process executes multiple execve calls
+        NODES[child_pid] = \
+            CCNode(child, parent=pnode, color=ccolor, pid=child_pid)
+
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+    # display process tree                   #
+    #                                        #
+    # TODO: extract into separate function   #
+    # TODO: better pruning of boring nodes   #
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
     # handle multiple roots
-    roots = [n for n in NODES.values() if n.is_root]
-
-    # DISPLAY PART
-    # TODO: extract function
-
-    sys.stdout.write(u"\u001b[" + str(1000) + "D")  # Move left
-    sys.stdout.write(u"\u001b[" + str(1000) + "A")  # Move up
-    for i, root in enumerate(roots):
+    roots = [n for n in NODES.values() if n.is_root and n.name != UNKNOWN_PROC_LABEL]
+    sys.stdout.write(u"\u001b[" + str(1000) + "D")  # move left
+    sys.stdout.write(u"\u001b[" + str(1000) + "A")  # move up
+    for root in roots:
         for pre, _, node in RenderTree(root, style=STY):
-            # skip over leaf calls to tools
-            # TODO: a bottom up walk would be a better pruning strat
+            # skip leaf nodes representing utility processes
             if node.is_leaf and node.color == DGRAY:
                 continue
-            line = "{}{}{} ({})".format(pre, node.color, node.name, node.count)
+            line = "{}{}{} ({})".format(pre, node.color, node.name, node.pid)
             line = line + NO_COLOR
-            # TODO: does not count escape codes or not?
+            # TODO: do manual justification to ignore escape codes
             line = line.ljust(COLUMNS)
             print(line)
-    # PP.pprint(NODES)
-    # if len(roots) > 2:
-    #     quit(1)
+       
+
+def handle_clone(exitevt: dict):
+    exepath, parent_pid = exitevt['proc.exepath'], int(exitevt['proc.ppid'])
+    child_pid = int(exitevt['proc.pid'])
+    assert child_pid > 0
+
+    assert parent_pid != child_pid
+
+    color = COLOR_MAP.get(exepath, NO_COLOR)
+    pnode = NODES.setdefault(parent_pid, 
+                             CCNode(exepath, color=color, pid=parent_pid))
+    cnode = NODES.setdefault(child_pid, 
+                             CCNode(exepath, parent=pnode, color=color, pid=child_pid))
 
 
 def main():
-
-    enter_events = dict()  # key'ed by thread id
-
     try:
         while True:
             line = sys.stdin.readline()
             evt = json.loads(line)  # type: dict
 
-            tid = int(evt['thread.tid'])
-            if evt['evt.dir'] == '>':  # enter events
-                enter_events[tid] = evt
-            else:  # exit events
-                assert evt['evt.dir'] == '<'
-                enter_event = enter_events.pop(tid)
-                handle_events(enter_event, evt)
+            if evt['evt.type'] == 'execve':
+                handle_execve(evt)
+            elif evt['evt.type'] == 'clone':  
+                # clone returns twice; once for parent and child.
+                if not "res=0 " in evt['evt.args']:
+                    continue  # ignore parent event
+                handle_clone(evt)
+
     except KeyboardInterrupt:
         sys.stdout.write("\n")
         sys.stdout.flush()
