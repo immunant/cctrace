beeefe2606a444da46b11293998aeb798e329e11
perl <per.larsen@gmail.com>
2018-06-08 01:37:28 -0700

    Refactor coloring of nodes.

--- ccevent.py
+++ ccevent.py
@@ -1,6 +1,97 @@
 # -*- coding: utf-8 -*-
 
 import re
+from collections import defaultdict
+
+class Colors:
+    # Terminal escape codes
+    DGRAY = '\033[90m'
+    FAIL = '\033[91m'
+    LGREEN = '\033[92m'
+    LYELLOW = '\033[93m'
+    LBLUE = '\033[94m'
+    NO_COLOR = '\033[0m'
+
+# Known binaries
+# TODO: use a tree-like structure here?
+COLOR_MAP = {
+    # utilities -> GRAY
+    '/bin/cat': Colors.DGRAY,
+    '/bin/uname': Colors.DGRAY,
+    '/bin/sh': Colors.DGRAY,
+    '/bin/ln': Colors.DGRAY,
+    '/bin/ls': Colors.DGRAY,
+    '/bin/mv': Colors.DGRAY,
+    '/bin/rm': Colors.DGRAY,
+    '/bin/mkdir': Colors.DGRAY,
+    '/bin/rmdir': Colors.DGRAY,
+    '/usr/bin/less': Colors.DGRAY,
+    '/usr/bin/sudo': Colors.DGRAY,
+    '/bin/bash': Colors.DGRAY,
+    '/bin/tar': Colors.DGRAY,
+    '/bin/date': Colors.DGRAY,
+    '/bin/gzip': Colors.DGRAY,
+    '/bin/sed': Colors.DGRAY,
+    '/bin/grep': Colors.DGRAY,
+    '/usr/bin/diff': Colors.DGRAY,
+    '/usr/bin/dpkg': Colors.DGRAY,
+    '/usr/bin/dpkg-query': Colors.DGRAY,
+    '/usr/bin/awk': Colors.DGRAY,
+    '/usr/bin/arch': Colors.DGRAY,
+    '/usr/bin/expr': Colors.DGRAY,
+    '/usr/bin/gawk': Colors.DGRAY,
+    '/usr/bin/basename': Colors.DGRAY,
+    '/usr/bin/env': Colors.DGRAY,
+    '/usr/bin/find': Colors.DGRAY,
+    '/usr/bin/nice': Colors.DGRAY,
+    '/usr/bin/sort': Colors.DGRAY,
+    '/usr/bin/print': Colors.DGRAY,
+    '/usr/bin/wget': Colors.DGRAY,
+    '/usr/bin/which': Colors.DGRAY,
+    '/usr/bin/touch': Colors.DGRAY,
+    # host translators -> LYELLOW
+    '/usr/bin/yasm': Colors.LYELLOW,
+    '/usr/bin/ar': Colors.LYELLOW,
+    '/usr/bin/as': Colors.LYELLOW,
+    '/usr/bin/x86_64-linux-gnu-as': Colors.LYELLOW,
+    '/usr/bin/ranlib': Colors.LYELLOW,
+    '/usr/bin/ld': Colors.LYELLOW,
+    '/usr/bin/gcc': Colors.LYELLOW,
+    # TODO: use regexes instead of per-version keys
+    '/usr/lib/gcc/x86_64-linux-gnu/4.8/cc1': Colors.LYELLOW,
+    '/usr/lib/gcc/x86_64-linux-gnu/4.8/cc1plus': Colors.LYELLOW,
+    '/usr/lib/gcc/x86_64-linux-gnu/4.8/collect2': Colors.LYELLOW,
+    '/usr/lib/gcc/x86_64-linux-gnu/7/cc1': Colors.LYELLOW,
+    '/usr/lib/gcc/x86_64-linux-gnu/7/cc1plus': Colors.LYELLOW,
+    '/usr/lib/gcc/x86_64-linux-gnu/7/collect2': Colors.LYELLOW,
+    '/usr/lib/llvm-3.6/bin/clang': Colors.LYELLOW,
+    '/usr/lib/llvm-3.6/bin/clang++': Colors.LYELLOW,
+    '/usr/bin/cc': Colors.LYELLOW,
+    '/usr/bin/g++': Colors.LYELLOW,
+    '/usr/bin/clang': Colors.LYELLOW,
+    '/usr/bin/clang++': Colors.LYELLOW,
+    # build tools
+    '/usr/bin/make': Colors.LBLUE,
+    '/usr/bin/cmake': Colors.LBLUE,
+    '/usr/bin/ccmake': Colors.LBLUE,
+    '/usr/bin/ctest': Colors.LBLUE,
+    '/usr/bin/cpack': Colors.LBLUE,
+    '/usr/bin/qmake': Colors.LBLUE,
+    '/usr/bin/scons': Colors.LBLUE,
+    '/usr/bin/ninja': Colors.LBLUE,
+    '/usr/bin/pkg-config': Colors.LBLUE,
+    '/usr/bin/bear': Colors.LBLUE,
+    # scripting engines -> BLUE
+    '/usr/bin/python2.7': Colors.LBLUE,
+    '/usr/bin/python3': Colors.LBLUE,
+    '/usr/bin/perl': Colors.LBLUE,
+    '/usr/bin/tclsh8.6': Colors.LBLUE,
+}
+
+
+def get_color(exepath: str) -> str:
+    color = COLOR_MAP.get(exepath, Colors.NO_COLOR)
+    return color
 
 
 def _parse_pid(s: str) -> int:
@@ -58,6 +149,10 @@ class CCEvent(object):
         self.ppid = ppid
         self.pargs = pargs
         self.eargs = eargs
+
+    @property
+    def color(self):
+        return get_color(self.exepath)
 
     @staticmethod
     def parse(line: str) -> object:
--- cctrace.py
+++ cctrace.py
@@ -11,102 +11,19 @@ from anytree import Node, RenderTree
 from anytree.render import AsciiStyle, ContStyle
 # from anytree.iterators.levelordergroupiter import LevelOrderGroupIter
 
-from ccevent import CCEvent
-
-# Terminal color codes
-NO_COLOR = '\033[0m'
-DGRAY = '\033[90m'
-FAIL = '\033[91m'
-LGREEN = '\033[92m'
-LYELLOW = '\033[93m'
-LBLUE = '\033[94m'
+from ccevent import CCEvent, Colors, get_color
 
 # Terminal dimensions
 COLUMNS, LINES = shutil.get_terminal_size((80, 20))
 
-# Known binaries
-# TODO: use a tree-like structure here?
-COLOR_MAP = {
-    # utilities -> GRAY
-    '/bin/cat': DGRAY,
-    '/bin/uname': DGRAY,
-    '/bin/sh': DGRAY,
-    '/bin/ln': DGRAY,
-    '/bin/ls': DGRAY,
-    '/bin/mv': DGRAY,
-    '/bin/rm': DGRAY,
-    '/bin/mkdir': DGRAY,
-    '/bin/rmdir': DGRAY,
-    '/usr/bin/less': DGRAY,
-    '/usr/bin/sudo': DGRAY,
-    '/bin/bash': DGRAY,
-    '/bin/tar': DGRAY,
-    '/bin/date': DGRAY,
-    '/bin/gzip': DGRAY,
-    '/bin/sed': DGRAY,
-    '/bin/grep': DGRAY,
-    '/usr/bin/diff': DGRAY,
-    '/usr/bin/dpkg': DGRAY,
-    '/usr/bin/dpkg-query': DGRAY,
-    '/usr/bin/awk': DGRAY,
-    '/usr/bin/arch': DGRAY,
-    '/usr/bin/expr': DGRAY,
-    '/usr/bin/gawk': DGRAY,
-    '/usr/bin/basename': DGRAY,
-    '/usr/bin/env': DGRAY,
-    '/usr/bin/find': DGRAY,
-    '/usr/bin/nice': DGRAY,
-    '/usr/bin/sort': DGRAY,
-    '/usr/bin/print': DGRAY,
-    '/usr/bin/wget': DGRAY,
-    '/usr/bin/which': DGRAY,
-    '/usr/bin/touch': DGRAY,
-    # host translators -> YELLOW
-    '/usr/bin/yasm': LYELLOW,
-    '/usr/bin/ar': LYELLOW,
-    '/usr/bin/as': LYELLOW,
-    '/usr/bin/x86_64-linux-gnu-as': LYELLOW,
-    '/usr/bin/ranlib': LYELLOW,
-    '/usr/bin/ld': LYELLOW,
-    '/usr/bin/gcc': LYELLOW,
-    # TODO: use regexes instead of per-version keys
-    '/usr/lib/gcc/x86_64-linux-gnu/4.8/cc1': LYELLOW,
-    '/usr/lib/gcc/x86_64-linux-gnu/4.8/cc1plus': LYELLOW,
-    '/usr/lib/gcc/x86_64-linux-gnu/4.8/collect2': LYELLOW,
-    '/usr/lib/gcc/x86_64-linux-gnu/7/cc1': LYELLOW,
-    '/usr/lib/gcc/x86_64-linux-gnu/7/cc1plus': LYELLOW,
-    '/usr/lib/gcc/x86_64-linux-gnu/7/collect2': LYELLOW,
-    '/usr/lib/llvm-3.6/bin/clang': LYELLOW,
-    '/usr/lib/llvm-3.6/bin/clang++': LYELLOW,
-    '/usr/bin/cc': LYELLOW,
-    '/usr/bin/g++': LYELLOW,
-    '/usr/bin/clang': LYELLOW,
-    '/usr/bin/clang++': LYELLOW,
-    # build tools
-    '/usr/bin/make': LBLUE,
-    '/usr/bin/cmake': LBLUE,
-    '/usr/bin/ccmake': LBLUE,
-    '/usr/bin/ctest': LBLUE,
-    '/usr/bin/cpack': LBLUE,
-    '/usr/bin/qmake': LBLUE,
-    '/usr/bin/scons': LBLUE,
-    '/usr/bin/ninja': LBLUE,
-    '/usr/bin/pkg-config': LBLUE,
-    '/usr/bin/bear': LBLUE,
-    # scripting engines -> BLUE
-    '/usr/bin/python2.7': LBLUE,
-    '/usr/bin/python3': LBLUE,
-    '/usr/bin/perl': LBLUE,
-    '/usr/bin/tclsh8.6': LBLUE,
-}
-COLOR_MAP = defaultdict(str, COLOR_MAP)
+
 PP = pprint.PrettyPrinter(indent=2)
 LANG_IS_UTF8 = os.environ.get('LANG', '').lower().endswith('utf-8')
 STY = ContStyle if LANG_IS_UTF8 else AsciiStyle
 SHELL = os.environ.get('SHELL', '').lower()
 
 UNKNOWN_PROC_LABEL = "[unknown executable]"
-UNKNOWN_PROC_COLOR = FAIL
+UNKNOWN_PROC_COLOR = Colors.FAIL
 
 SYSDIG_NA = '<NA>'
 
@@ -116,6 +33,11 @@ roots = set()  # holds root nodes, never shrinks
 
 class CCNode(Node):
     separator = "|"
+
+    @property
+    def color(self):
+        return get_color(self.name)
+
 
     def hash_subtree(self):
         res = hash(self.name)
@@ -153,14 +75,14 @@ def print_tree():
             else:
                 duplicates.add(marker)
             # skip leaf nodes representing utility processes
-            if node.is_leaf and node.color == DGRAY:
+            if node.is_leaf and node.color == Colors.DGRAY:
                 continue
             # skip child nodes of configure calls
             if node.parent and node.parent.name.endswith("configure"):
                 continue
             # line = "{}{}{}".format(pre, node.color, node.name)
             line = "{}{}{} ({})".format(pre, node.color, node.name, node.pid)
-            line = line + NO_COLOR
+            line = line + Colors.NO_COLOR
             # TODO: do manual justification to ignore escape codes
             forrest.append(line.ljust(COLUMNS))
 
@@ -194,22 +116,18 @@ def handle_execve(exitevt: CCEvent):
             print(exitevt.eargs)
             assert False
 
-    ccolor = COLOR_MAP.get(child, NO_COLOR)
-
     # the lookup of the parent process can fail if the process was
     # started before we started running sysdig
-    rnode = CCNode(UNKNOWN_PROC_LABEL,
-                   color=UNKNOWN_PROC_COLOR, pid=parent_pid)
+    rnode = CCNode(UNKNOWN_PROC_LABEL, pid=parent_pid)
     pnode = nodes_by_pid.setdefault(parent_pid, rnode)
 
     cnode = nodes_by_pid.get(child_pid, None)
     if cnode:
         cnode.name = child
-        cnode.color = ccolor
     else:
         # happens if a process executes multiple execve calls
         nodes_by_pid[child_pid] = \
-            CCNode(child, parent=pnode, color=ccolor, pid=child_pid)
+            CCNode(child, parent=pnode, pid=child_pid)
 
     print_tree()
 
@@ -219,12 +137,11 @@ def handle_clone(exitevt: CCEvent):
     assert child_pid > 0, "Unexpected child pid: {}".format(child_pid)
     assert parent_pid != child_pid
 
-    color = COLOR_MAP.get(exitevt.exepath, NO_COLOR)
     pnode = nodes_by_pid.setdefault(parent_pid,
-                                    CCNode(exitevt.exepath, color=color, pid=parent_pid))
+                                    CCNode(exitevt.exepath, pid=parent_pid))
     cnode = nodes_by_pid.setdefault(child_pid, None)
     if not cnode:
-        nodes_by_pid[child_pid] = CCNode(exitevt.exepath, parent=pnode, color=color, pid=child_pid)
+        nodes_by_pid[child_pid] = CCNode(exitevt.exepath, parent=pnode, pid=child_pid)
 
     # sanity check that all children have distinct pid's
     # child_pids = set()
