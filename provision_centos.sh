e30483503c06ce978e7ce648ebd1bac6b4db563e
Per <perl@immunant.com>
2018-07-18 01:22:01 +0000

    Update python3 provisioning for CentOS.

--- provision_centos.sh
+++ provision_centos.sh
@@ -17,11 +17,12 @@ yum -y check-update
 yum -yt install yum-utils 
 yum -yt install https://centos7.iuscommunity.org/ius-release.rpm
 yum -yt install python36u python36u-pip
+sudo ln -fs /usr/bin/python3.6 /usr/bin/python3
+sudo ln -fs /usr/bin/pip3.6 /usr/bin/pip3
 # anytree python library
-pip3.6 install --quiet --user --no-cache anytree
+pip3 install --quiet --user --no-cache anytree
 
 # conditionally install sysdig
 if ! type "sysdig" > /dev/null 2>&1; then
-  # don't warn when adding developer GPG key
   curl -s https://s3.amazonaws.com/download.draios.com/stable/install-sysdig | bash
 fi
